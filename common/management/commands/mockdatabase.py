e925ae106edbfb7cc7b0a6d9746516149a4452de
michael <longforward@gmail.com>
2012-10-19 19:52:08 +0800

    add solarcell into it

--- common/management/commands/mockdatabase.py
+++ common/management/commands/mockdatabase.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from turbine.models import Turbine, TurbineAlarm, TurbineData, TurbineState
+from solarcell.models import Solarcell, SolarcellAlarm, SolarcellData, SolarcellState
 import random
 import time
 
@@ -22,5 +23,21 @@ class Command(BaseCommand):
                                                      alarm=random.choice(alarms),
                                                      state=random.choice(states))
             dataadded += 1
+            #self.stdout.write('"dataadded:%d"' % dataadded)
+            time.sleep(1)
+
+    def handle(self, *args, **options):
+        alarms = list(SolarcellAlarm.objects.all())
+        states = list(SolarcellState.objects.all())
+
+        solarcell = list(Solarcell.objects.all())
+        dataadded1 = 0
+        while(True):
+            solarcelldata = SolarcellData.objects.create(solarcell=random.choice(solarcell),
+                                                         status=random.choice([True, False]),
+                                                         power=random.randint(0, 600),
+                                                         alarm=random.choice(alarms),
+                                                         state=random.choice(states))
+            dataadded1 += 1
             #self.stdout.write('"dataadded:%d"' % dataadded)
             time.sleep(1)
